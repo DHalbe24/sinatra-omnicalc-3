35b00aa603616852517a20c6c5eac4acc83fc3c9
DHalbe24 <137205368+DHalbe24@users.noreply.github.com>
2023-07-20 00:25:43 +0000

    initial working now

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
   "
@@ -7,3 +8,64 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/umbrella") do
+  erb(:umbrella_form)
+end
+
+post("/process_umbrella") do
+  @user_location = params.fetch("user_loc")
+
+  url_encoded_string = @user_location.gsub(" ", "+")
+
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{url_encoded_string}&key=AIzaSyA2sS7veAf82D7PNeDC-n1NuWD5BUMSxd0"
+
+  @raw_response = HTTP.get(gmaps_url).to_s
+
+  @parsed_response = JSON.parse(@raw_response)
+
+  @loc_hash = @parsed_response.dig("results", 0, "geometry", "location")
+
+  @latitude = @loc_hash.fetch("lat")
+  @longitude = @loc_hash.fetch("lng")
+
+  erb(:umbrella_results)
+end
+
+
+
+
+
+'''
+require "sinatra"
+require "sinatra/reloader"
+require "http"
+
+get("/") do
+  "
+  <p>Welcome to OmniCalc 3</p>
+  "
+end
+
+get("/umbrella") do
+  erb(:umbrella_form)
+end
+
+get("/process_umbrella") do
+  @user_location = params.fetch("user_loc")
+  url_encoded_string = @user_location.gsub(" ", "+")
+
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{url_encoded_string}&key=AIzaSyA2sS7veAf82D7PNeDC-n1NuWD5BUMSxd0"
+  
+  @raw_response = HTTP.get(gmaps_url).to_s
+
+  @parsed_response = JSON.parse(@raw_response)
+
+  @loc_hash = @parsed_response.dig("results", 0, "geometry", "location")
+
+  @latitude = @loc_hash.fetch("lat")
+  @longitude = @loc_hash.fetch("lng")
+
+  erb(:umbrella_results)
+end
+'''
